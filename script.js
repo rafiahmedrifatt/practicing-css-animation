4d5dff1ad402c1323db7ff741f02172d8b5c728c
rafiahmedrifatt <rafiahmed0471@gmail.com>
2025-01-04 10:51:32 +0600

    some js

---
+++ script.js
@@ -0,0 +1,59 @@
+// const product = {
+//     name : " Ball Pen",
+//     deal: 4,
+//     price: 270,
+//     offer: 5
+// }
+
+//let age = 2
+
+//if(age > 18) {
+//  console.log('you can vote')
+// } else {
+//     console.log('go make NID first')
+// }
+
+// let number = 25;
+
+// if (number%5){
+//     console.log('yo')
+// }
+
+// FOR LOOPS
+
+for (let i = 1; i <= 5; i++) {
+  console.log("hey good morning");
+}
+// WHILE LOOP
+
+let i = 1;
+while (i <= 5) {
+  console.log("yo");
+  i++;
+}
+
+// Do while loop
+
+do {
+  console.log("hello there");
+  i++;
+} while (i <= 20);
+
+// FOR-OF LOOP
+
+let str = "HELLO WORLD";
+let size = 0;
+for (const i of str) {
+  console.log(i);
+  size++;
+}
+
+// FOR IN LOOP
+
+let student = {
+  name: "rafi ahmed",
+  roll: 394,
+  age: 45,
+  class: 33,
+  isPassed: true,
+};
