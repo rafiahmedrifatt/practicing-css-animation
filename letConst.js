e05f92450b42de2d9dd1500e4e1db1327c6bb760
rafiahmedrifatt <rafiahmed0471@gmail.com>
2025-01-04 07:29:10 +0600

    first commit

---
+++ letConst.js
@@ -0,0 +1,21 @@
+// const product = {
+//     name : " Ball Pen",
+//     deal: 4,
+//     price: 270,
+//     offer: 5
+// }
+
+
+//let age = 2
+
+//if(age > 18) {
+//  console.log('you can vote')
+// } else {
+//     console.log('go make NID first')
+// }
+
+let number = 25;
+
+if (number%5){
+    console.log('yo')
+}
