e07312e11600a5b75da085e1e7b2cd32abbf9dad
rafiahmedrifatt <rafiahmed0471@gmail.com>
2025-02-06 14:17:02 +0600

    added few tasks

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+// TASK 01
 
+var orangePrice = 300;
+var applePrice = 400;
+var moneyGiven = 1000;
+var total = orangePrice + applePrice;
+
+var returnMoney = moneyGiven - total;
+
+console.log(returnMoney);
+
+// TASK 02
+
+var mathematics = 75.2545345;
+var Biology = 60.4345348;
+var physics = 94.834545434;
+var chemistry = 84.8890235444;
+var bengal = 43.059345;
+
+var average = [mathematics + Biology + physics + chemistry + bengal / 5];
+
+var n = average.toFixed(2);
+
+console.log(n);
+
+// TASK 3
+
+var givenNumber = 119;
+var x = 119 % 5;
+
+console.log(x);
