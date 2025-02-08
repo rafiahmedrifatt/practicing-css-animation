e510b81b6f3c75c06ba4ad5c60d51ec653b7b086
rafiahmedrifatt <rafiahmed0471@gmail.com>
2025-02-09 00:50:42 +0600

    loop added

---
+++ js-loop.js
@@ -0,0 +1,14 @@
+const number = [12, 98, 93, 38, 39, 29, 48, 38];
+
+console.log(number);
+
+for (const num of number) {
+  console.log(num);
+}
+
+const fruits = ["orange", "apple", "mango", "jackfruits"];
+
+for (const fruit of fruits) {
+  console.log(fruit);
+  console.log("i want to eat every fruits");
+}
