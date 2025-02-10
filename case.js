4ce369abe84c68ed762e70c5d9bae3560c25342b
rafiahmedrifatt <rafiahmed0471@gmail.com>
2025-02-10 18:04:06 +0600

    case and strings added

---
+++ case.js
@@ -0,0 +1,24 @@
+const school = "Rajuk uttara model school";
+
+console.log(school.toUpperCase());
+console.log(school.toLowerCase());
+
+// uPPER CASE
+const subject = "Chemistry";
+// LOWER CASE
+const book = "chemistry";
+
+if (subject.toLowerCase() === book.toLowerCase()) {
+  console.log("i am reading book aibar porikkhay fatai felmu");
+} else {
+  console.log(" hudai hudai prista ultai vat khawar jnno");
+}
+
+const drink = "wa ter";
+const liquid = "water "; //space after water
+
+if (drink.trim() === liquid.trim()) {
+  console.log("pani r opor nam jibon");
+} else {
+  console.log("yoooooo pani");
+}
---
+++ string.js
@@ -0,0 +1,24 @@
+const country = "bangladesh";
+const division = "newkhali";
+const district = `b-baria`;
+// const thana = new String("demra ");
+
+// console.log(thana);
+
+const numbers = [54, 98, 78, 21, 65];
+// console.log(numbers);
+// console.log(numbers.length);
+// console.log(numbers[0]);
+// numbers[1] = 11;
+// console.log(numbers);
+
+// string is immutable ---> Not changeable
+// array is mutable --> array is mutable
+
+const capital = "Dhaka";
+// console.log(capital.length);
+// console.log(capital[0]);
+
+console.log(capital);
+capital[1] = "F";
+console.log();
